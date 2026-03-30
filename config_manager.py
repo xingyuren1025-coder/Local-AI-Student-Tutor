517eb76f7e0e3a6299f703f133de76d2322e3f5d
xingyuren1025-coder <xingyuren1025@gmail.com>
2026-03-31 01:30:11 +0300

    Update config_manager.py

--- config_manager.py
+++ config_manager.py
@@ -1,4 +1,4 @@
-import os
+=import os
 
 MODEL_NAME = "Qwen/Qwen2.5-0.5B-Instruct"
 
@@ -7,13 +7,15 @@ LOG_FILE = os.path.join(BASE_DIR, "chatbot_logs.csv")
 INSTRUCTION_FILE = os.path.join(BASE_DIR, "chatbot_instructions.txt")
 
 # 函数 1：读取指令
+# Read Instruction
 def load_instructions():
     if not os.path.exists(INSTRUCTION_FILE):
         return "You are a professional academic AI tutor. Explain technical concepts clearly."
     with open(INSTRUCTION_FILE, "r", encoding="utf-8") as f:
         return f.read().strip()
 
-# 函数 2：保存指令 (你可能漏掉了这个函数)
+# 函数 2：保存指令
+# Save Command
 def save_instructions(new_text):
     with open(INSTRUCTION_FILE, "w", encoding="utf-8") as f:
         f.write(new_text)
